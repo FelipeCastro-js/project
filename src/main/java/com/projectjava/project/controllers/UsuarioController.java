87fea3bc1964bea112dfffa94fbef315ed235166
Felipe Castro <carloscastro1860@gmail.com>
2025-07-22 08:38:32 -0500

    feat: initialize repository with the initial project configuration

---
+++ src/main/java/com/projectjava/project/controllers/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.projectjava.project.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value= "prueba")
+    public List prueba(){
+        return List("prueba");
+    }
+}
