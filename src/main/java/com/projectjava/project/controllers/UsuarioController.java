d0dab2554533b7c28132f2f42444fd9df5fea494
Felipe Castro <carloscastro1860@gmail.com>
2025-07-22 21:12:01 -0500

    feat: Add layout of the users table and create instances for the users controller

--- src/main/java/com/projectjava/project/controllers/UsuarioController.java
+++ src/main/java/com/projectjava/project/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.projectjava.project.controllers;
 
+import com.projectjava.project.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,14 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value= "prueba")
-    public List prueba(){
-        return List("prueba");
+    @RequestMapping(value= "usuario/id")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setName("Felipe");
+        usuario.setLastname("Castro");
+        usuario.setEmail("carloscastro1860@gmail.com");
+
+        return usuario;
     }
 }
